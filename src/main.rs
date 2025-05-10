e8718ac63870fea553bff30e003e48c16ba82145
macgeargear <macgeargear@gmail.com>
2025-05-10 20:37:03 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,56 @@
 mod model;
-mod ui;
+mod msg;
+mod update;
+mod view;
 
-use crate::model::{Deck, FlashCard};
+use std::io::stdout;
 
 use color_eyre::eyre::Result;
-use ui::{run_app, select_dect};
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{EnterAlternateScreen, enable_raw_mode},
+};
+use model::{Model, Screen};
+use msg::Msg;
+use ratatui::{Terminal, prelude::CrosstermBackend};
+use update::update;
+use view::view;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+    enable_raw_mode()?;
 
-    let decks = vec![
-        Deck {
-            name: "General Knowledge".into(),
-            flashcards: vec![
-                FlashCard {
-                    front: "What is the capital of France?".into(),
-                    back: "Paris".into(),
-                },
-                FlashCard {
-                    front: "What does 'ownership' mean in Rust?".into(),
-                    back: "It means a variable owns memory and is responsible for freeing it."
-                        .into(),
-                },
-            ],
-        },
-        Deck {
-            name: "Rust Syntax".into(),
-            flashcards: vec![FlashCard {
-                front: "What is a slice?".into(),
-                back: "&[T] — a view into a sequence".into(),
-            }],
-        },
-    ];
+    let mut stdout = stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    let selected_deck = select_dect(&decks)?;
-    run_app(selected_deck)
+    let mut model = Model::default();
+
+    loop {
+        terminal.draw(|f| view(&model, f))?;
+
+        if let Event::Key(key) = event::read()? {
+            let msg = match key.code {
+                KeyCode::Char('q') => Msg::Quit,
+                KeyCode::Char('b') => Msg::BacktoSelectDeck,
+                KeyCode::Char(' ') => Msg::FlipCard,
+                KeyCode::Left => Msg::PrevCard,
+                KeyCode::Right => Msg::NextCard,
+                KeyCode::Up => match model.screen {
+                    Screen::DeckSelection => Msg::NavigateDeckUp,
+                    _ => continue,
+                },
+                KeyCode::Down => match model.screen {
+                    Screen::DeckSelection => Msg::NavigateDeckDown,
+                    _ => continue,
+                },
+                KeyCode::Enter => Msg::ConfirmDeck,
+                _ => continue,
+            };
+
+            model = update(msg, model);
+        }
+    }
 }
